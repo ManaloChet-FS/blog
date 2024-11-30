40aa30d7058d33382de656bac7c0a2db15731896
ManaloChet-FS <manalochet01@gmail.com>
2024-11-30 13:23:11 -0500

    Added generateStaticParams

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { getAllPosts } from "@/lib/posts"
+import { getAllPosts } from "@/lib/posts";
 import { notFound } from "next/navigation";
 import MarkdownIt from "markdown-it";
 import Link from "next/link";
@@ -11,16 +11,16 @@ async function fetchPost(slug: string) {
 }
 
 interface Params {
-  slug: string
+  slug: string;
 }
 
 interface Post {
-  title: string
-  date: string
-  content: string
+  title: string;
+  date: string;
+  content: string;
 }
 
-export default async function Post({params}: { params: Params }) {
+export default async function Post({ params }: { params: Params }) {
   const post = await fetchPost(params.slug);
 
   if (!post) notFound();
@@ -30,14 +30,29 @@ export default async function Post({params}: { params: Params }) {
 
   return (
     <article className="text-slate-50 mx-auto p-4 max-w-4xl">
-      <Link href="/" className="inline-block mb-2 transition bg-slate-200 text-zinc-900 font-semibold py-2 px-4 rounded-full hover:bg-zinc-700 hover:text-slate-50">&#11164; Go back</Link>
-      <section className='bg-zinc-900 shadow-lg p-8 rounded-lg border border-zinc-800'>
+      <Link
+        href="/"
+        className="inline-block mb-2 transition bg-slate-200 text-zinc-900 font-semibold py-2 px-4 rounded-full hover:bg-zinc-700 hover:text-slate-50"
+      >
+        &#11164; Go back
+      </Link>
+      <section className="bg-zinc-900 shadow-lg p-8 rounded-lg border border-zinc-800">
         <div className="flex justify-between items-center mb-8">
           <h1 className="text-4xl font-semibold">{title}</h1>
           <span className="text-lg">{date}</span>
         </div>
-        <div className="text-lg" dangerouslySetInnerHTML={{ __html: htmlConverter }} />
+        <div
+          className="text-lg"
+          dangerouslySetInnerHTML={{ __html: htmlConverter }}
+        />
       </section>
     </article>
-  )
-} 
+  );
+}
+
+export async function generateStaticParams() {
+  const posts = getAllPosts();
+  return posts.map((post) => ({
+    slug: post.slug,
+  }));
+}
